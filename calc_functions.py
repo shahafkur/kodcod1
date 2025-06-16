b6f62f280af0f289806a7191c53dfc62980a929f
shahafkur <shahafkurman0@gmail.com>
2025-06-16 12:01:57 +0300

    sec commit

---
+++ calc_functions.py
@@ -0,0 +1,18 @@
+
+
+
+def calculations(a,b):
+    listi = [a*b,a-b]
+    print(listi)
+    return listi
+
+
+def sum_all(listi):
+    sumi = sum(listi)
+    return sumi
+
+
+
+def funci(a):
+    return a+5
+
